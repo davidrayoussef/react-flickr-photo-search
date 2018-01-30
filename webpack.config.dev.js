28446e4e03e0ad1e74777a7ec85138ef81aff823
David Ra Youssef <davidradeveloper@gmail.com>
2018-01-30 10:50:36 -0500

    Install file-loader and url-loader

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -14,13 +14,21 @@ module.exports = {
 	module: {
 		rules: [
       {
-				enforce: "pre",
+				enforce: 'pre',
         test: /\.js$/,
 				exclude: /node_modules/,
 				use: [
-          "babel-loader",
-          "eslint-loader",
+          'babel-loader',
+          'eslint-loader',
         ]
+      },
+			{
+        test: /\.(png|jpg|gif)$/,
+        use: ['url-loader']
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+        loader: 'file-loader?name=assets/[name].[hash].[ext]'
       },
 			{
 				test: /\.js$/,
