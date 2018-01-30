26ed63e1089f0a6ee5f2cd823f806d7cb1654e01
David Ra Youssef <davidradeveloper@gmail.com>
2018-01-30 10:51:54 -0500

    Add dummy data

--- src/components/FlickrApp.js
+++ src/components/FlickrApp.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
+import SearchBox from './SearchBox';
 import PropTypes from 'prop-types';
 
 class FlickrApp extends Component {
-  constructor(props) {
-    super();
+  state = {
+    photos: [
+      { src: 'dogs1.jpg' },
+      { src: 'dogs2.jpg' },
+      { src: 'dogs3.jpg' },
+      { src: 'dogs4.jpg' }
+    ]
   }
 
   render() {
+    const { photos } = this.state;
+    const renderPhotos = photos.map(photo => (
+      <img key={photo.src} src={require(`../images/${photo.src}`)} />
+    ));
+
     return (
       <div className="wrapper">
-        <header>
-          <input placeholder="Search" />
-        </header>
+        <SearchBox />
         <main>
           <img style={{border: '1px solid black', height: 500 }} />
         </main>
         <footer>
-          <img style={{border: '1px solid black'}} />
-          <img style={{border: '1px solid black'}} />
-          <img style={{border: '1px solid black'}} />
-          <img style={{border: '1px solid black'}} />
+          {renderPhotos}
         </footer>
       </div>
     );
