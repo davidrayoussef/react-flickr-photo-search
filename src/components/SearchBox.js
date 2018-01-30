f5a3d738ab4c22ca577b91b3a89509cd3e2581a8
David Ra Youssef <davidradeveloper@gmail.com>
2018-01-30 10:52:20 -0500

    Add SearchBox component

---
+++ src/components/SearchBox.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const SearchBox = () => {
+  return (
+    <header>
+      <input placeholder="Search" />
+    </header>
+  );
+}
+
+SearchBox.propTypes = {
+
+};
+
+export default SearchBox;
